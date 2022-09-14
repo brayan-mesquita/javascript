199faee5a724b1afeb9915ae755f495b890d1b57
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-09-14 00:03:41 -0400

    aulas

--- loops.js
+++ loops.js
@@ -1,1 +1,10 @@
 //https://youtu.be/_VDaHe0HlFI
+for (let index = 1; index < 10; index++){
+    console.log(index);
+}
+
+//loop em lista
+const carros = ["fusca","kombi","onix",'chevet'];
+for (let i = 0; i < carros.length; i++){
+    console.log(carros[i])
+}
