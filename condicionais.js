468b64c603fd750bc561c12380ae884890c9c5ee
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-09-16 23:34:30 -0400

    Update condicionais.js

--- condicionais.js
+++ condicionais.js
@@ -1,5 +1,35 @@
-a = 4 + 3
 
-if (a < 10) {
-    alert('menor')    
+const a = 1 + 1;
+const b = 2 + 2
+
+if(a != 1+2){
+    console.log("A difere de 1 + 2")
+}
+if (a == 1 || b != a) {
+    console.log("Segunda condição atendida")
+}
+if (a == 1+1 && b != a) {
+    console.log("Terceira condição atendida")
+}
+
+// atribuindo valor a variavel a partir de condicao
+let number;
+function numero(){
+    if(a == 2){
+        number = 2
+    }else{
+        number = 0}
+    console.log(number)}
+numero()
+
+// exeplo case em carros
+
+const car = ["fusca", "kombi", "onix"]
+switch(car[1]){
+    case "fusca":
+        console.log("o carro é fusca")
+        break
+    case "kombi":
+        console.log("o carro é kombi")
+        break
 }
