f23524a8fc0b78131d7696e7c455b849d2f03d24
Brayan Soares <brayanmesquita@gmail.com>
2022-09-19 23:05:06 -0400

    brayan-Inspiron-7472, 2022-09-19

--- condicionais.js
+++ condicionais.js
@@ -1,6 +1,6 @@
 
-const a = 1 + 1;
-const b = 2 + 2
+const a = 1 + 1;//2
+const b = 2 + 2;//4
 
 if(a != 1+2){
     console.log("A difere de 1 + 2")
@@ -15,14 +15,14 @@ if (a == 1+1 && b != a) {
 // atribuindo valor a variavel a partir de condicao
 let number;
 function numero(){
-    if(a == 2){
+    if(a != 2){
         number = 2
     }else{
-        number = 0}
+        number = 10}
     console.log(number)}
 numero()
 
-// exeplo case em carros
+// exemplo case em carros
 
 const car = ["fusca", "kombi", "onix"]
 switch(car[1]){
@@ -33,3 +33,33 @@ switch(car[1]){
         console.log("o carro é kombi")
         break
 }
+
+
+//testando valores logicos de variáveis
+function teste(variavel){
+    if(variavel){
+        console.log('passou')
+    }
+    else{
+        console.log('não passou')
+    }
+}
+teste(!null)
+teste(3)
+teste(!3)//exclação invert sinal
+
+
+//testar lista vazia
+let carros = ['onix', 'fusca']
+let motos = []
+
+function listaocupada(x){
+    if (x.length > 0){
+        console.log(`${x} tem valor`)
+    }
+    else{
+        console.log(`${x.name} não tem valor`)
+    }
+}
+listaocupada(carros)
+listaocupada(motos)
